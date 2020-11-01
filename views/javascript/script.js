e490f60ce543442ee8f47bed6e1f6afca1c825b0
Evelyn Fredin <evefre2001@skola.goteborg.se>
2020-11-01 16:24:54 +0100

    Updated Js file, now heart style can be toggled

--- views/javascript/script.js
+++ views/javascript/script.js
@@ -2,8 +2,14 @@
    ========================================================================== */
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const hearts = document.getElementsByClassName('fa-heart');
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
+
+for(let i = 0; i < hearts.length; i++){
+    hearts[i].addEventListener('click', () => {
+        hearts[i].classList.toggle("red");
+    });
+};
